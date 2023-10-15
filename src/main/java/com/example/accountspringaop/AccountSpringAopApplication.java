1a49d76e805cd2e5da6e7b0e1d7921167a30fe32
Aakarsh Sinha <aakarshsak@gmail.com>
2023-10-16 00:44:53 +0530

    Initializing

---
+++ src/main/java/com/example/accountspringaop/AccountSpringAopApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountspringaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountSpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountSpringAopApplication.class, args);
+	}
+
+}
